56a7954f28366930183a1649c7a50f466c53fbd3
FancyFennec <jonasallemann@gmx.ch>
2020-02-09 16:12:17 +0100

    got the entity manager to work

--- src/test/java/parallel/steps/ExerciseDAO.java
+++ src/test/java/parallel/steps/ExerciseDAO.java
@@ -3,13 +3,18 @@ package parallel.steps;
 import lombok.NoArgsConstructor;
 import org.hibernate.Session;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 @NoArgsConstructor
 public class ExerciseDAO {
 
+    EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("persistence");
+    EntityManager entityManager = entityManagerFactory.createEntityManager();
+
     public Exercise getExerciseById(Integer id) {
-        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-            return session.createQuery("from Exercise where id = : id", Exercise.class)
-                    .setParameter("id", id).getResultList().get(0);
-        }
+        return entityManager.createQuery("from Exercise where id = : id", Exercise.class)
+                .setParameter("id", id).getResultList().get(0);
     }
 }
